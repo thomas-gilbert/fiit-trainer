2d8166ea6c2f6bed76d596a445c9e6ebb37e571b
Thomas Gilbert <Thomas.Gilbert@burberry.com>
2017-09-29 20:06:42 +0100

    Updated folder structure
    Added simple routing
    
    Added 404 page

---
+++ src/containers/home/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './index.css';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <div className="lander">
+          <h1>FIIT Trainer</h1>
+          <p>A programmable HIIT Trainer</p>
+        </div>
+      </div>
+    );
+  }
+}
