6b8232045da55c359e021877a0488a34c13df594
Thomas Gilbert <thomas.gilbert@uk.tesco.com>
2017-10-04 11:22:15 +0100

    Updated get call to use query rather than get

--- fiit-trainer-api/get.js
+++ fiit-trainer-api/get.js
@@ -2,20 +2,17 @@ import * as dynamoDbLib from './libs/dynamodb-lib';
 import { success, failure } from './libs/response-lib';
 
 export async function main(event, context, callback) {
-  console.log('hi');
   const params = {
     TableName: 'exercises',
-    // 'Key' defines the partition key and sort key of the item to be retrieved
-    // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'noteId': path parameter
-    Key: {
-      exerciseName:  null,
-      exerciseType: "Cardio"
-    }
+    KeyConditionExpression: 'exerciseType = :type'
+    ExpressionAttributeValues: {
+      ':type': event.exerciseType
+    },
+    ProjectionExpression: 'exerciseName',
   };
 
   try {
-    const result = await dynamoDbLib.call('get', params);
+    const result = await dynamoDbLib.call('query', params);
 
     if (result.Item) {
       // Return the retrieved item
--- fiit-trainer-api/libs/dynamodb-lib.js
+++ fiit-trainer-api/libs/dynamodb-lib.js
@@ -3,7 +3,6 @@ import AWS from 'aws-sdk';
 AWS.config.update({ region: 'eu-west-2' });
 
 export function call(action, params) {
-	console.log('hi');
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
   return dynamoDb[action](params).promise();
